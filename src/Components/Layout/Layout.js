de176890acef2f7f3114308832f8209ea2cf612c
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-19 14:51:10 +0700

    Part 8.7 done

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
 
 function layout(props) {
     return (
         <Auxiliary>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>{props.children}</main>
+            <main className={classes.Content}>{props.children}</main>
         </Auxiliary>
     );
 }
