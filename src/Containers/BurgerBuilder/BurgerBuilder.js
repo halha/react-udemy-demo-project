4fe8ab9f479786b88b71bf6d06feb756b501961a
Hammam Al-Hakim <hammam_al_27rpl@student.smktelkom-mlg.sch.id>
2020-02-24 13:28:18 +0700

    Part 8.12 done

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from "../../Components/Burger/Burger";
 export class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
+            salad: 0,
+            bacon: 0,
             cheese: 2,
-            meat: 2
+            meat: 0
         }
     };
 
